fd7c1281e0e619193104bf146dea43480bdc762b
xime.duran <xime.duran@hotmail.com>
2012-04-20 21:13:40 +0000

    un par de constructores

--- MEREditor/src/mereditor/modelo/base/Componente.java
+++ MEREditor/src/mereditor/modelo/base/Componente.java
@@ -3,4 +3,12 @@ package mereditor.modelo.base;
 public abstract class Componente {
 	String idComponente;
 	String idContenedor;
+	
+	public Componente (String idComp, String idCont ) {
+		idComponente= idComp;
+		idContenedor= idCont;
+	}
+	public Componente () {
+		
+	}
 }
