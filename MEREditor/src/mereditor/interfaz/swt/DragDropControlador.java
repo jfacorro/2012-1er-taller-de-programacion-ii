73911db97630f9450f0772af477f2c2aede1f132
juan.facorro <juan.facorro@61df381b-933a-9c06-0957-03833d59ff3f>
2012-05-02 20:53:17 +0000

    Agregado de ejemplo de arrastre de elemento con SWT.

--- MEREditor/src/mereditor/interfaz/swt/DragDropControlador.java
+++ MEREditor/src/mereditor/interfaz/swt/DragDropControlador.java
@@ -1,57 +1,58 @@
 package mereditor.interfaz.swt;
 
+import org.eclipse.draw2d.Figure;
 import org.eclipse.draw2d.MouseEvent;
 import org.eclipse.draw2d.MouseListener;
 import org.eclipse.draw2d.MouseMotionListener;
+import org.eclipse.draw2d.geometry.Point;
 
 public class DragDropControlador implements MouseListener, MouseMotionListener {
+	private Point startPoint;
 
-	@Override
-	public void mouseDragged(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	public DragDropControlador() {
+
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	public void mouseDragged(MouseEvent evt) {
+		if (this.startPoint != null) {
+			this.actualizarPosicion((Figure)evt.getSource(), this.startPoint, evt.getLocation());
+			this.startPoint = new Point(evt.getLocation());
+		}
 	}
 
 	@Override
-	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	public void mouseEntered(MouseEvent evt) {}
+
+	@Override
+	public void mouseExited(MouseEvent evt) {}
+
+	@Override
+	public void mouseHover(MouseEvent evt) {}
+
+	@Override
+	public void mouseMoved(MouseEvent evt) {}
+
+	@Override
+	public void mouseDoubleClicked(MouseEvent arg0) {}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		this.startPoint = new Point(e.getLocation());
 	}
 
 	@Override
-	public void mouseHover(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	public void mouseReleased(MouseEvent e) {
+		this.startPoint = null;
 	}
+	
+	public void actualizarPosicion(Figure figure, Point start, Point current) {
+		if (!start.equals(current)) {
+			int dx = current.x - start.x;
+			int dy = current.y - start.y;
 
-	@Override
-	public void mouseMoved(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
+			Point location = figure.getLocation();
+			figure.setLocation(new Point(location.x + dx, location.y + dy));
+		}		
 	}
-
-	@Override
-	public void mouseDoubleClicked(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
