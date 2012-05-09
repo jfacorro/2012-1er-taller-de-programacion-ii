5d24766cdb387584b7e72adb917cf8d3785bca6d
xime.duran <xime.duran@hotmail.com>
2012-05-09 03:17:09 +0000

    ejemplo jerarquia y menu

--- MEREditor/src/SWTTest/Snippet2.java
+++ MEREditor/src/SWTTest/Snippet2.java
@@ -1,23 +1,5 @@
 package SWTTest;
 
-/*******************************************************************************
- * Copyright (c) 2000, 2004 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
-
-/* 
- * example snippet: Hello World
- *
- * For a list of all SWT example snippets see
- * http://www.eclipse.org/swt/snippets/
- */
-
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
@@ -27,7 +9,7 @@ import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.*;
 
-public class Snippet1 {
+public class Snippet2 {
 	
 	
 	static void crearMenu(Shell shell) {
