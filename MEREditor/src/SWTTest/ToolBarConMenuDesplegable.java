4d9113a920cd61dcfa17ad62e3489703b303f41f
xime.duran <xime.duran@hotmail.com>
2012-05-12 22:45:25 +0000

    Menu desplegable en las opciones

---
+++ MEREditor/src/SWTTest/ToolBarConMenuDesplegable.java
@@ -0,0 +1,26 @@
+package SWTTest;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.ToolBar;
+import org.eclipse.swt.widgets.ToolItem;
+
+public class ToolBarConMenuDesplegable {
+	ToolBar toolBar;
+	
+	public ToolBarConMenuDesplegable (Composite c, int modo){
+		toolBar= new ToolBar (c,modo);
+	}
+	
+	public void agregarItem (String textoItem, Menu itemMenu) {
+		ToolItem item= new ToolItem(toolBar,SWT.DROP_DOWN);
+		item.setText(textoItem);
+		item.addListener(SWT.Selection, new ToolBarListener(item,toolBar,itemMenu) );
+	}
+	
+	public ToolBar getToolBar() {
+		return toolBar;
+	}
+	
+}
