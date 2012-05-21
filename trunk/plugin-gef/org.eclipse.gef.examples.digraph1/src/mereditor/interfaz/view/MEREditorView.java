5a71869c87a25a000169ed0681d6ef37d43bfec7
hordiales@gmail.com <hordiales@gmail.com@61df381b-933a-9c06-0957-03833d59ff3f>
2012-05-21 05:50:20 +0000

    * Generando plugin GEF para el MEREditor

---
+++ trunk/plugin-gef/org.eclipse.gef.examples.digraph1/src/mereditor/interfaz/view/MEREditorView.java
@@ -0,0 +1,54 @@
+package mereditor.interfaz.view;
+
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.gef.DefaultEditDomain;
+import org.eclipse.gef.GraphicalViewer;
+import org.eclipse.gef.editparts.FreeformGraphicalRootEditPart;
+import org.eclipse.gef.examples.digraph1.factory.Digraph1EditPartFactory;
+import org.eclipse.gef.examples.digraph1.model.Digraph1Graph;
+import org.eclipse.gef.ui.parts.ScrollingGraphicalViewer;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class MEREditorView extends ViewPart {
+
+	private DefaultEditDomain editDomain;
+	private GraphicalViewer graphicalViewer;
+
+	@Override
+	public void createPartControl(Composite parent) {
+
+		setEditDomain(new DefaultEditDomain(null));
+		setGraphicalViewer(new ScrollingGraphicalViewer());
+		getGraphicalViewer().createControl(parent);
+		getGraphicalViewer().setRootEditPart(
+				new FreeformGraphicalRootEditPart());
+		getGraphicalViewer().setEditPartFactory(new Digraph1EditPartFactory());
+		getGraphicalViewer().setContents(new Digraph1Graph());
+		getGraphicalViewer().getControl().setBackground(
+				ColorConstants.listBackground);
+	}
+
+	protected DefaultEditDomain getEditDomain() {
+		return this.editDomain;
+	}
+
+	protected GraphicalViewer getGraphicalViewer() {
+		return this.graphicalViewer;
+	}
+
+	protected void setEditDomain(DefaultEditDomain anEditDomain) {
+		this.editDomain = anEditDomain;
+	}
+
+	@Override
+	public void setFocus() {
+		getGraphicalViewer().getControl().setFocus();
+	}
+
+	protected void setGraphicalViewer(GraphicalViewer viewer) {
+		getEditDomain().addViewer(viewer);
+		this.graphicalViewer = viewer;
+	}
+
+}
