1327dc24fd8ab5a3ce6f1b3e8d70f6bc3ba4fe46
juan.facorro <juan.facorro@61df381b-933a-9c06-0957-03833d59ff3f>
2012-05-30 15:34:52 +0000

    Actualización del DTD según las modificaciones realizadas.

--- trunk/MEREditor/src/mereditor/tests/ParserTest.java
+++ trunk/MEREditor/src/mereditor/tests/ParserTest.java
@@ -1,4 +1,5 @@
 package mereditor.tests;
+
 import java.io.File;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -10,28 +11,28 @@ import mereditor.modelo.Diagrama;
 import mereditor.parser.Parser;
 import junit.framework.TestCase;
 
-
 public class ParserTest extends TestCase {
 
 	private static final String PATH_ARCHIVO_PARSERTEST = "src/mereditor/tests/xml de prueba/boleteria-comp.xml";
 	private Parser p;
-	
+
 	protected void setUp() throws Exception {
 		super.setUp();
-		File source = new File (PATH_ARCHIVO_PARSERTEST);		
-		DocumentBuilder builder= DocumentBuilderFactory.newInstance().newDocumentBuilder();
-		Document doc= builder.parse(source);
-		p= new Parser (doc);
+		File source = new File(PATH_ARCHIVO_PARSERTEST);
+		DocumentBuilder builder = DocumentBuilderFactory.newInstance()
+				.newDocumentBuilder();
+		Document doc = builder.parse(source);
+		p = new Parser(doc);
 	}
 
 	public void testParsear() {
 		p.parsear();
-		Diagrama ppal= p.getDiagramaPpal();
-		assertTrue (ppal!= null);
-		assertTrue (ppal.getIdComponente().equals("99"));
-		assertTrue (ppal.getNombre().equals("BoleteriaTeatro"));
-		assertTrue (ppal.getDiagramas().isEmpty());
-		assertTrue (ppal.getComponentes().size()== 7);
+		Diagrama ppal = p.getDiagramaPpal();
+		assertTrue(ppal != null);
+		assertTrue(ppal.getIdComponente().equals("_99"));
+		assertTrue(ppal.getNombre().equals("BoleteriaTeatro"));
+		assertTrue(ppal.getDiagramas().isEmpty());
+		assertTrue(ppal.getComponentes().size() == 7);
 	}
 
 }
