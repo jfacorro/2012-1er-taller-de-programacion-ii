3f34256a929bdcda43b7ae9b0a71643cfa79c619
xime.duran@hotmail.com <xime.duran@hotmail.com@61df381b-933a-9c06-0957-03833d59ff3f>
2012-06-12 14:27:01 +0000

    Cortar y Pegar

--- trunk/MEREditor/src/mereditor/interfaz/swt/listeners/MenuArbolControlador.java
+++ trunk/MEREditor/src/mereditor/interfaz/swt/listeners/MenuArbolControlador.java
@@ -16,35 +16,75 @@ import org.eclipse.swt.widgets.TreeItem;
 public class MenuArbolControlador implements Listener{
 
 	Tree observado;
+	String treeItemCortadoData;
+	TreeItem treeItemActivo;
 	Menu menu;
 	
 	public MenuArbolControlador(Tree tree, Menu m){
 		observado= tree;
 		menu= m;
 		observado.addListener(SWT.MouseDown, this);
+		treeItemCortadoData= null;
+		treeItemActivo= null;
+		agregarMenuListeners ();
 	}
 	
 	public void handleEvent(Event event) {
 		Point point = new Point (event.x, event.y);
 		TreeItem treeItem= observado.getItem(point);
 		if (treeItem != null){
+			treeItemActivo= treeItem;
 			Rectangle area= treeItem.getBounds();
 			Point p= new Point (area.x, area.y + area.height);
 			p= observado.toDisplay(p);
 			menu.setLocation(p.x, p.y);
 			menu.setVisible(true);
-			menu.setData(treeItem);
-			agregarMenuListeners (treeItem);
 		}
 	}
 
-	private void agregarMenuListeners(TreeItem treeItem) {
+	private void agregarMenuListeners() {
 		MenuItem[] items = menu.getItems();
 		for (int i=0; i<items.length;i++){
-			new ItemMenuArbolControlador (items[i],treeItem);
-		}
-		
+			new ItemMenuArbolControlador (items[i]);
+		}	
 	}
 	
+	
+	private class ItemMenuArbolControlador implements Listener {
 
+		private static final String ACCION_ELIMINAR = "Eliminar";
+		private static final String ACCION_CORTAR = "Cortar";
+		private static final String ACCION_PEGAR = "Pegar";
+		
+		
+		
+		public ItemMenuArbolControlador (MenuItem menuItem) {
+			menuItem.addListener(SWT.Selection, this);
+		}
+
+		public void handleEvent(Event event) {
+			MenuItem menuItem=(MenuItem) event.widget;
+			if (menuItem == null)
+				return;
+			String accion=menuItem.getText();
+			if (accion.equals(ACCION_ELIMINAR)) {
+				treeItemActivo.dispose();
+			}
+			else
+			if (accion.equals(ACCION_CORTAR)) {
+				treeItemCortadoData= treeItemActivo.getText();
+				menuItem.setText(ACCION_PEGAR);
+				treeItemActivo.dispose();
+				treeItemActivo= null;
+			}
+			else
+			if (accion.equals(ACCION_PEGAR)) {
+				TreeItem nuevoItem = new TreeItem (treeItemActivo,SWT.NULL);
+				nuevoItem.setText(treeItemCortadoData);
+				menuItem.setText(ACCION_CORTAR);
+			}
+		}
+
+	}
+	
 }
