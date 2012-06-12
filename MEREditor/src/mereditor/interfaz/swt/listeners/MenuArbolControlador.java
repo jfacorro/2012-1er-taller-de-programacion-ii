b7794e3d055c41409caa6ffbd3ffd3020d34c131
xime.duran <xime.duran@hotmail.com>
2012-06-12 00:53:38 +0000

    eliminar del arbol

--- MEREditor/src/mereditor/interfaz/swt/listeners/MenuArbolControlador.java
+++ MEREditor/src/mereditor/interfaz/swt/listeners/MenuArbolControlador.java
@@ -1,11 +1,13 @@
 package mereditor.interfaz.swt.listeners;
 
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeItem;
 
@@ -19,20 +21,30 @@ public class MenuArbolControlador implements Listener{
 	public MenuArbolControlador(Tree tree, Menu m){
 		observado= tree;
 		menu= m;
-		tree.addListener(SWT.MouseDown, this);
+		observado.addListener(SWT.MouseDown, this);
 	}
 	
 	public void handleEvent(Event event) {
 		Point point = new Point (event.x, event.y);
-		TreeItem item= observado.getItem(point);
-		if (item != null){
-			Rectangle area= item.getBounds();
+		TreeItem treeItem= observado.getItem(point);
+		if (treeItem != null){
+			Rectangle area= treeItem.getBounds();
 			Point p= new Point (area.x, area.y + area.height);
 			p= observado.toDisplay(p);
 			menu.setLocation(p.x, p.y);
 			menu.setVisible(true);
-			menu.setData(item);
+			menu.setData(treeItem);
+			agregarMenuListeners (treeItem);
 		}
 	}
+
+	private void agregarMenuListeners(TreeItem treeItem) {
+		MenuItem[] items = menu.getItems();
+		for (int i=0; i<items.length;i++){
+			new ItemMenuArbolControlador (items[i],treeItem);
+		}
+		
+	}
+	
 
 }
