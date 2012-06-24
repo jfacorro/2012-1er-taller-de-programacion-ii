1ea31cb7d028ea2fa0987c86efbd572f1be7ab34
juan.facorro <juan.facorro@61df381b-933a-9c06-0957-03833d59ff3f>
2012-06-24 20:20:07 +0000

    Agregado de métodos cargarDatos y aplicarCambios en Editor. Agregado de métodos de creación de combos y textbox con sus labels.

--- MEREditor/src/mereditor/interfaz/swt/editores/Editor.java
+++ MEREditor/src/mereditor/interfaz/swt/editores/Editor.java
@@ -10,16 +10,22 @@ import mereditor.modelo.Jerarquia.TipoJerarquia;
 import mereditor.modelo.Relacion.TipoRelacion;
 import mereditor.modelo.base.Componente;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
-public class Editor<T extends Componente> extends Dialog {
+public abstract class Editor<T extends Componente> extends Dialog {
 	public final static String[] TiposAtributo = getTipos(TipoAtributo.class);
 	public final static String[] TiposEntidades = getTipos(TipoEntidad.class);
 	public final static String[] TiposRelaciones = getTipos(TipoRelacion.class);
 	public final static String[] TiposJerarquias = getTipos(TipoJerarquia.class);
-	
+
 	protected T componente;
-	
+
 	/**
 	 * Constructor para el editor visual.
 	 */
@@ -32,12 +38,55 @@ public class Editor<T extends Componente> extends Dialog {
 		this.componente = componente;
 	}
 	
+	@Override
+	protected Control createContents(Composite parent) {
+		Control control = super.createContents(parent);
+		this.cargarDatos();
+
+		return control;
+	}
+
 	private static <T> String[] getTipos(Class<T> enumClass) {
 		java.util.List<String> tipos = new ArrayList<>();
-		
-		for(T tipo : enumClass.getEnumConstants())
+
+		for (T tipo : enumClass.getEnumConstants())
 			tipos.add(tipo.toString());
-		
+
 		return tipos.toArray(new String[] {});
 	}
+
+	@Override
+	protected void okPressed() {
+		this.aplicarCambios();
+		principal.actualizarVista();
+		super.okPressed();
+	}
+	
+	protected static Text createLabelText(Composite parent, String name) {
+		Label lblNombre = new Label(parent, SWT.LEFT);
+		lblNombre.setText(name);
+
+		Text txtField = new Text(parent, SWT.BORDER);
+		
+		return txtField;
+	}
+	
+	protected static Combo createLabelCombo(Composite parent, String name) {
+		Label lblNombre = new Label(parent, SWT.LEFT);
+		lblNombre.setText(name);
+
+		Combo cboField = new Combo(parent, SWT.BORDER);
+		
+		return cboField;
+	}
+	
+	/**
+	 * Carga los datos del componente en los controles.
+	 */
+	protected abstract void cargarDatos();
+
+	/**
+	 * Aplica los cambios hechos al componente.
+	 */
+	protected abstract void aplicarCambios();
 }
