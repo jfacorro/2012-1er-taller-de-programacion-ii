d42877e30af51e3d502dba658795a1f5ca4088ef
juan.facorro <juan.facorro@61df381b-933a-9c06-0957-03833d59ff3f>
2012-06-24 11:03:17 +0000

    Modificación del dialogo.

--- MEREditor/src/mereditor/interfaz/swt/dialogs/Dialog.java
+++ MEREditor/src/mereditor/interfaz/swt/dialogs/Dialog.java
@@ -2,20 +2,19 @@ package mereditor.interfaz.swt.dialogs;
 
 import mereditor.interfaz.swt.Principal;
 
-import org.eclipse.jface.window.Window;
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
 
-public abstract class Dialog extends Window {
+public abstract class Dialog extends org.eclipse.jface.dialogs.Dialog {
 	protected Principal principal = Principal.getInstance();
 	protected String titulo = "";
-	
+
 	/**
 	 * @wbp.parser.constructor
 	 */
@@ -27,46 +26,39 @@ public abstract class Dialog extends Window {
 	public Dialog() {
 		super(Principal.getInstance().getShell());
 	}
-
+	
 	@Override
 	protected Control createContents(Composite parent) {
-		this.getShell().setText(this.titulo);
+		Control control = super.createContents(parent);
 		
-		Composite compositeButtons = new Composite(parent, SWT.NONE);
-		
-		GridLayout gridLayout = new GridLayout(2, false);
-		gridLayout.verticalSpacing = 2;
-		gridLayout.marginWidth = 2;
-		gridLayout.marginHeight = 2;
-		gridLayout.marginBottom = 2;
-		gridLayout.horizontalSpacing = 2;
-		compositeButtons.setLayout(gridLayout);
-
-		Button btnOK = new Button(compositeButtons, SWT.PUSH);
+		Button btnOK = this.getButton(IDialogConstants.OK_ID);
+		Button btnCancel = this.getButton(IDialogConstants.CANCEL_ID);
 		btnOK.setText("Aceptar");
 		btnOK.addSelectionListener(this.aceptar);
-
-		Button btnCancel = new Button(compositeButtons, SWT.PUSH);
 		btnCancel.setText("Cancelar");
 		btnCancel.addSelectionListener(this.cancelar);
 		
-		this.getShell().pack();
-
-		return super.createContents(parent);
+		return control;
 	}
 	
+	@Override
+	protected void configureShell(Shell newShell) {
+		super.configureShell(newShell);
+		newShell.setText(this.titulo);
+	}
+
 	/**
 	 * Acciones a realizar al presionar el boton aceptar
 	 */
 	protected abstract void aceptar();
-	
+
 	/**
 	 * Acciones a realizar al presionar el boton cancelar
 	 */
 	protected void cancelar() {
 		this.close();
 	}
-	
+
 	private SelectionAdapter aceptar = new SelectionAdapter() {
 		public void widgetSelected(SelectionEvent e) {
 			aceptar();
