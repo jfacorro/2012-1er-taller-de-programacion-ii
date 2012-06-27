7278496228e6a7c2b62fa9a9195924857ee93f3a
juan.facorro <juan.facorro@61df381b-933a-9c06-0957-03833d59ff3f>
2012-06-27 22:30:24 +0000

    Agregado de validacion al RelacionEditor.

--- MEREditor/src/mereditor/interfaz/swt/editores/RelacionEditor.java
+++ MEREditor/src/mereditor/interfaz/swt/editores/RelacionEditor.java
@@ -102,6 +102,13 @@ public class RelacionEditor extends Editor<Relacion> {
 
 	@Override
 	protected boolean validar(List<String> errors) {
-		return errors.size() == 0;
+		boolean valido = true;
+
+		if (txtNombre.getText().trim().length() == 0) {
+			errors.add("Debe completar el nombre.");
+			valido = false;
+		}
+
+		return valido;
 	}
 }
