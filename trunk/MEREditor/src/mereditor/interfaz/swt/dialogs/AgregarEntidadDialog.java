ce55061bc504dbe41c501ba3a4a9f2643b09bf38
juan.facorro <juan.facorro@61df381b-933a-9c06-0957-03833d59ff3f>
2012-07-10 03:55:24 +0000

    Modificación del diálogo para agrrgar una nueva entidad a un diagrama.

--- trunk/MEREditor/src/mereditor/interfaz/swt/dialogs/AgregarEntidadDialog.java
+++ trunk/MEREditor/src/mereditor/interfaz/swt/dialogs/AgregarEntidadDialog.java
@@ -1,8 +1,5 @@
 package mereditor.interfaz.swt.dialogs;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Set;
 
 import mereditor.interfaz.swt.editores.Editor;
@@ -13,16 +10,12 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 
 public class AgregarEntidadDialog extends Dialog {
-	private Combo cboEntidades;
-	private Map<String, Entidad> entidades = new HashMap<>();
-	private Entidad entidad = null;	
+	private Entidad entidad = null;
 
 	/**
 	 * @wbp.parser.constructor
@@ -40,17 +33,17 @@ public class AgregarEntidadDialog extends Dialog {
 	protected Control createDialogArea(Composite parent) {
 		Composite container = (Composite) super.createDialogArea(parent);
 
-		Label lblEntidades = new Label(container, SWT.LEFT);
-		lblEntidades.setText("Entidades");
-
-		this.cboEntidades = new Combo(container, SWT.READ_ONLY);
-		this.cboEntidades.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		this.loadEntidades();
+		Button btnSeleccionar = new Button(container, SWT.PUSH);
+		btnSeleccionar.setText("Seleccionar Existente");
+		btnSeleccionar.addSelectionListener(this.seleccionar);
+		btnSeleccionar.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true,
+				false, 1, 1));
 
 		Button btnNueva = new Button(container, SWT.PUSH);
 		btnNueva.setText("Nueva Entidad");
 		btnNueva.addSelectionListener(this.nueva);
-		btnNueva.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
+		btnNueva.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false,
+				1, 1));
 
 		return container;
 	}
@@ -59,40 +52,25 @@ public class AgregarEntidadDialog extends Dialog {
 	 * Carga las entidades que pertenecen al diagrama actual y a sus padres en
 	 * el combo.
 	 */
-	private void loadEntidades() {
+	private Set<Entidad> loadEntidades() {
 		// Obtener las entidades de los ancestros
-		Set<Entidad> entidades = this.principal.getProyecto().getEntidadesDisponibles();
-		Set<Entidad> entidadesDiagrama = this.principal.getProyecto().getEntidadesDiagrama();
+		Set<Entidad> entidades = this.principal.getProyecto()
+				.getEntidadesDisponibles();
+		Set<Entidad> entidadesDiagrama = this.principal.getProyecto()
+				.getEntidadesDiagrama();
 		// Quitar las que ya tiene
 		entidades.removeAll(entidadesDiagrama);
 
-		for (Entidad entidad : entidades) {
-			this.entidades.put(entidad.getNombre(), entidad);
-			this.cboEntidades.add(entidad.getNombre());
-		}
-
-		String[] items = this.cboEntidades.getItems();
-		Arrays.sort(items);
-		this.cboEntidades.setItems(items);
+		return entidades;
 	}
-	
+
 	/**
 	 * Devuelve la entidad seleccionada o creada.
+	 * 
 	 * @return
 	 */
 	public Entidad getComponente() {
 		return this.entidad;
-	}
-
-	@Override
-	protected void okPressed() {
-		if (cboEntidades.getSelectionIndex() == -1) {
-			this.principal.error("No seleccionó ninguna entidad");
-		} else {
-			String nombre = cboEntidades.getText();
-			entidad = this.entidades.get(nombre);
-			super.okPressed();
-		}
 	}
 
 	protected SelectionAdapter nueva = new SelectionAdapter() {
@@ -100,6 +78,17 @@ public class AgregarEntidadDialog extends Dialog {
 			Editor<Entidad> editor = new EntidadEditor();
 			int result = editor.open();
 			entidad = editor.getComponente();
+			setReturnCode(result);
+			close();
+		};
+	};
+
+	protected SelectionAdapter seleccionar = new SelectionAdapter() {
+		public void widgetSelected(org.eclipse.swt.events.SelectionEvent e) {
+			SeleccionarComponenteDialog<Entidad> dialog = new SeleccionarComponenteDialog<Entidad>(
+					loadEntidades());
+			int result = dialog.open();
+			entidad = dialog.getComponente();
 			setReturnCode(result);
 			close();
 		};
